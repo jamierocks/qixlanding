77f7821b2d3ee8650f7c79241ffbe7c9d8b0e6ac
Jamie Mansfield <dev@jamierocks.uk>
2016-03-19 12:44:14 +0000

    Use cssmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 var serve = require('gulp-serve');
 
 gulp.task('scss', function () {
@@ -8,10 +10,17 @@ gulp.task('scss', function () {
         .pipe(gulp.dest('./assets/css'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch('scss/*.scss', ['scss']);
+gulp.task('build', ['scss'], function () {
+    return gulp.src('./assets/css/application.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('watch', ['build'], function () {
+    gulp.watch('scss/*.scss', ['build']);
 });
 
 gulp.task('serve', ['watch'], serve('.'));
 
-gulp.task('default', ['scss']);
+gulp.task('default', ['build']);
